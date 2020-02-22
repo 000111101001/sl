f004e2a91b8e41beb4acb169e425bed04ee21be7
000111101001 <45956614+000111101001@users.noreply.github.com>
2020-02-23 05:54:21 +0700

    Update encrypt.js

--- encrypt.js
+++ encrypt.js
@@ -1,13 +1,13 @@
 if(window.addEventListener){
- window.addEventListener('load', function(){
-  for(i = 0; i &lt; document.getElementsByClassName('post-body')[0].getElementsByTagName('a').length; i++){
-   document.getElementsByClassName('post-body')[0].getElementsByTagName('a')[i].href = openSite() + Base64.encode(document.getElementsByClassName('post-body')[0].getElementsByTagName('a')[i].href);
+ window.addEventListener(&#39;load&#39;, function(){
+  for(i = 0; i &lt; document.getElementsByClassName(&#39;post-body&#39;)[0].getElementsByTagName(&#39;a&#39;).length; i++){
+   document.getElementsByClassName(&#39;post-body&#39;)[0].getElementsByTagName(&#39;a&#39;)[i].href = openSite() + Base64.encode(document.getElementsByClassName(&#39;post-body&#39;)[0].getElementsByTagName(&#39;a&#39;)[i].href);
   }
  }, false);
 }else{
- window.attachEvent('onload', function(){
-  for(i = 0; i &lt; document.getElementsByClassName('post-body')[0].getElementsByTagName('a').length; i++){
-   document.getElementsByClassName('post-body')[0].getElementsByTagName('a')[i].href = openSite() + Base64.encode(document.getElementsByClassName('post-body')[0].getElementsByTagName('a')[i].href);
+ window.attachEvent(&#39;onload&#39;, function(){
+  for(i = 0; i &lt; document.getElementsByClassName(&#39;post-body&#39;)[0].getElementsByTagName(&#39;a&#39;).length; i++){
+   document.getElementsByClassName(&#39;post-body&#39;)[0].getElementsByTagName(&#39;a&#39;)[i].href = openSite() + Base64.encode(document.getElementsByClassName(&#39;post-body&#39;)[0].getElementsByTagName(&#39;a&#39;)[i].href);
   }
  });
 }
