0caee269ed4f77bae9ebe7857d9571a3da7d7b5f
000111101001 <45956614+000111101001@users.noreply.github.com>
2020-02-23 07:11:17 +0700

    Add files via upload

---
+++ bt.js
@@ -0,0 +1,4 @@
+document.getElementById("anuAchor").addEventListener("click", function(){
+    document.getElementById("anuDL").style.display = "block";
+    document.getElementById("anuAchor").style.display = "none";
+});
